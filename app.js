f2618ad0935e61ca023b1e52e6415f38a2f7391d
Shan-leo <bizwithshan@gmail.com>
2021-01-06 20:16:56 +0530

    update for uploading heroku

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure", function (req, res) {
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 30000");
 });
 
